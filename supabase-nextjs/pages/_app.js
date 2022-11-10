facf4fa9a0f908a6fd23f94fdfee0e1410872b9c
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2022-11-11 01:12:46 +0600

    connect

--- supabase-nextjs/pages/_app.js
+++ supabase-nextjs/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import {useState} from 'react'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 
@@ -6,12 +7,14 @@ function MyApp({ Component, pageProps }) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
 
   return (
-    <SessionContextProvider
+
+     <SessionContextProvider
       supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
+      initialSession={pageProps.initialSession}>
       <Component {...pageProps} />
     </SessionContextProvider>
+  
+   
   )
 }
 export default MyApp
